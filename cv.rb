94491a4cd851179319ca8e1374f5703c3a5c41ae
hyqooo <andreykostenko99@gmail.com>
2019-02-27 10:34:11 +0300

    cv start

---
+++ cv.rb
@@ -0,0 +1,25 @@
+require_relative "read_input"
+
+def correct_age?(age)
+  case age
+  when 15..99
+    return true
+  else
+    return false
+  end
+end
+
+def main
+  name = read_input_string("Input name and surename: ")
+  puts "#{name}"
+  email = read_input_string("Input email: ")
+  puts "#{email}"
+
+
+  begin
+    age = read_input_number("Input your age: ")
+  end until correct_age?(age)
+
+end
+
+main
