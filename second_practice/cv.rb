3e1fdcd8eef7059c44a86b5bcc8dd3d9f53babc2
hyqooo <tenwand@gmail.com>
2019-03-02 12:38:08 +0300

    CV Condition statements.

--- second_practice/cv.rb
+++ second_practice/cv.rb
@@ -10,16 +10,39 @@ def correct_age?(age)
 end
 
 def main
-  name = read_input_string("Input name and surename: ")
-  puts "#{name}"
-  email = read_input_string("Input email: ")
-  puts "#{email}"
+  puts "Fill in all fields and get your profession"
+  profession = ""
 
+  name = read_input_string("Input name and surename: ")
+  email = read_input_string("Input email: ")
 
   begin
     age = read_input_number("Input your age: ")
   end until correct_age?(age)
 
+  experience = read_input_number("Input your work experience: ")
+
+  if name.strip == "Pyotr Petrovich"
+    profession = "Chief"
+  else
+    if email.include?("code")
+      profession = "Engineer"
+    elsif experience < 2
+      profession = "Intern"
+    elsif age >= 45 && age < 60 
+      profession = "Veteran"
+    end
+  end
+
+  if experience > 5
+    profession = "Famous ".concat(profession)
+  end
+
+  if experience > 15  
+    profession = "Honored ".concat(profession)
+  end
+   
+  puts "Your profession: #{profession}"
 end
 
 main
