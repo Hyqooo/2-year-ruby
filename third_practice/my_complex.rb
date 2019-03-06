89edf60cbf85bee36ab2a05154a51c78151cade9
hyqooo <andreykostenko99@gmail.com>
2019-03-06 09:40:41 +0300

    complex number

--- third_practice/my_complex.rb
+++ third_practice/my_complex.rb
@@ -1,30 +1,30 @@
 class MyComplex
   attr_reader :real, :imaginary
   
-  def initialize (real, imaginary)
+  def initialize(real, imaginary)
     @real = Float(real)
     @imaginary = Float(imaginary)
   end
 
-  def add (other)
+  def add(other)
     real = @real + other.real
     imaginary = @imaginary + other.imaginary
     MyComplex.new(real, imaginary)
   end
 
-  def sub (other)
+  def sub(other)
     real = @real - other.real
     imaginary = @imaginary - other.imaginary
     MyComplex.new(real, imaginary)
   end
 
-  def multiply (other)
+  def multiply(other)
     real = @real * other.real - @imaginary * other.imaginary
     imaginary = @real * other.imaginary + @imaginary * other.real
     MyComplex.new(real, imaginary)
   end
 
-  def divide (other)
+  def divide(other)
     newComplexNumber = multiply(MyComplex.new(other.real, -other.imaginary)) 
     real = newComplexNumber.real / (other.real * other.real + other.imaginary * other.imaginary)
     imaginary = newComplexNumber.imaginary / (other.real * other.real + other.imaginary * other.imaginary)
